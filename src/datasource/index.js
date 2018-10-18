344975d229cc7ffc51a890b430414cf4f455405b
Daniel Sanchez <jdaniel.saed@gmail.com>
2018-10-18 13:31:40 -0500

    remove unused files and deps

--- src/datasource/index.js
+++ src/datasource/index.js
@@ -1,13 +1,9 @@
 /**
- * So, the project currently supports mongo db OR a google spreadsheet as a datasource
- */
-
-/**
  * This function is not necessary, I just wanted to log
  * to the console which ds is being used.
  */
 function selectedDatasource() {
-  const options = ["mongo", "graphcms", "spreadsheet"];
+  const options = ["graphcms", "spreadsheet"];
   const selected = options[1];
   console.info(`Using --- ${selected} --- as database.`);
   return selected;
