636e26b13376f751fd3e641b0c7ae69e5a93c7e0
Daniel Sanchez <jdaniel.saed@gmail.com>
2018-10-22 17:53:45 -0500

    add apollo engine

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 const { ApolloServer } = require("apollo-server");
 const getRemoteExecutableSchema = require("./schema");
 
+const { APOLLO_ENGINE_APIKEY } = process.env;
+
 async function start() {
   const schema = await getRemoteExecutableSchema();
 
-  const server = new ApolloServer({ schema, introspection: true });
+  const server = new ApolloServer({
+    schema,
+    introspection: true,
+    engine: {
+      apiKey: APOLLO_ENGINE_APIKEY
+    }
+  });
 
   server.listen().then(({ url }) => {
     console.log(`🚀  Server ready at ${url}`);
